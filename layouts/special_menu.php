d753a44d9018b52438b09c40ccb904eb79f47e4d
Francis Gonzales <fgonzalestello91@gmail.com>
2016-02-28 14:44:44 -0500

    Translation support

--- layouts/special_menu.php
+++ layouts/special_menu.php
@@ -2,29 +2,29 @@
   <li>
     <a href="home.php">
       <i class="glyphicon glyphicon-home"></i>
-      <span>Dashboard</span>
+      <span><?php  echo text()->translate('Dashboard'); ?></span>
     </a>
   </li>
   <li>
     <a href="categorie.php" >
       <i class="glyphicon glyphicon-indent-left"></i>
-      <span>Categorie</span>
+      <span><?php  echo text()->translate('Categorie'); ?></span>
     </a>
   </li>
   <li>
     <a href="#" class="submenu-toggle">
       <i class="glyphicon glyphicon-th-large"></i>
-      <span>Product</span>
+      <span><?php  echo text()->translate('Product'); ?></span>
     </a>
     <ul class="nav submenu">
-       <li><a href="product.php">Manage product</a> </li>
-       <li><a href="add_product.php">Add product</a> </li>
+       <li><a href="product.php"><?php  echo text()->translate('Manage product'); ?></a> </li>
+       <li><a href="add_product.php"><?php  echo text()->translate('Add product'); ?></a> </li>
    </ul>
   </li>
   <li>
     <a href="media.php" >
       <i class="glyphicon glyphicon-picture"></i>
-      <span>Media</span>
+      <span><?php  echo text()->translate('Media'); ?></span>
     </a>
   </li>
 </ul>
--- layouts/user_menu.php
+++ layouts/user_menu.php
@@ -2,28 +2,28 @@
   <li>
     <a href="home.php">
       <i class="glyphicon glyphicon-home"></i>
-      <span>Dashboard</span>
+      <span><?php  echo text()->translate('Dashboard'); ?></span>
     </a>
   </li>
   <li>
     <a href="#" class="submenu-toggle">
       <i class="glyphicon glyphicon-th-list"></i>
-       <span>Sales</span>
+       <span><?php  echo text()->translate('Sales'); ?></span>
       </a>
       <ul class="nav submenu">
-         <li><a href="sales.php">Manage Sales</a> </li>
-         <li><a href="add_sale.php">Add Sale</a> </li>
+         <li><a href="sales.php"><?php  echo text()->translate('Manage Sales'); ?></a> </li>
+         <li><a href="add_sale.php"><?php  echo text()->translate('Add Sale'); ?></a> </li>
      </ul>
   </li>
   <li>
     <a href="#" class="submenu-toggle">
       <i class="glyphicon glyphicon-signal"></i>
-       <span>Sales Report</span>
+       <span><?php  echo text()->translate('Sales Report'); ?></span>
       </a>
       <ul class="nav submenu">
-        <li><a href="sales_report.php">Sales by dates </a></li>
-        <li><a href="monthly_sales.php">Monthly sales</a></li>
-        <li><a href="daily_sales.php">Daily sales</a> </li>
+        <li><a href="sales_report.php"><?php  echo text()->translate('Sales by dates'); ?> </a></li>
+        <li><a href="monthly_sales.php"><?php  echo text()->translate('Monthly sales'); ?></a></li>
+        <li><a href="daily_sales.php"><?php  echo text()->translate('Daily sales'); ?></a> </li>
       </ul>
   </li>
 </ul>
